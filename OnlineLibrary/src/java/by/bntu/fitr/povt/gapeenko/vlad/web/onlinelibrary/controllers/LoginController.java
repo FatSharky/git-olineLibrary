e31a9c9d7094088707daa46f0a259cd748dd5731
FatSharky <vladislavbypinsk@mail.ru>
2015-11-08 19:24:21 +0300

    Creating LoginCotroller

---
+++ OnlineLibrary/src/java/by/bntu/fitr/povt/gapeenko/vlad/web/onlinelibrary/controllers/LoginController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package by.bntu.fitr.povt.gapeenko.vlad.web.onlinelibrary.controllers;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Vladislav
+ */
+@ManagedBean
+@RequestScoped
+public class LoginController {
+
+    /**
+     * Creates a new instance of LoginController
+     */
+    public LoginController() {
+    }
+    
+    public String login(){
+        return "books";
+    }
+    
+}
