c1a02d4150034b5977a54d5928b8ae85915d70c6
FatSharky <vladislavbypinsk@mail.ru>
2015-11-08 15:04:42 +0300

    Implements interface validator

--- OnlineLibrary/src/java/by/bntu/fitr/povt/gapeenko/vlad/web/onlinelibrary/validators/LoginValidator.java
+++ OnlineLibrary/src/java/by/bntu/fitr/povt/gapeenko/vlad/web/onlinelibrary/validators/LoginValidator.java
@@ -5,10 +5,27 @@
  */
 package by.bntu.fitr.povt.gapeenko.vlad.web.onlinelibrary.validators;
 
+import java.util.ResourceBundle;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
 /**
  *
  * @author Vladislav
  */
-public class LoginValidator {
+public class LoginValidator implements Validator{
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        if (value.toString().length()<= 5){
+            ResourceBundle bundle = ResourceBundle.getBundle("by.bntu.fitr.povt.gapeenko.vlad.web.onlinelibrary.nls.messages", FacesContext.getCurrentInstance().getViewRoot().getLocale());
+            FacesMessage message = new FacesMessage(bundle.getString("login_length_error"));
+            message.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(message);
+        }
+    }
     
 }
