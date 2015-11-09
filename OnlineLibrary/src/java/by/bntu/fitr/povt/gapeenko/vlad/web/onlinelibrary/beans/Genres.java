e813733d9f9af51c6b9349559fb5abf79ff4e0d5
FatSharky <vladislavbypinsk@mail.ru>
2015-11-09 19:25:57 +0300

    Fix some bugs

--- OnlineLibrary/src/java/by/bntu/fitr/povt/gapeenko/vlad/web/onlinelibrary/beans/Genres.java
+++ OnlineLibrary/src/java/by/bntu/fitr/povt/gapeenko/vlad/web/onlinelibrary/beans/Genres.java
@@ -5,21 +5,77 @@
  */
 package by.bntu.fitr.povt.gapeenko.vlad.web.onlinelibrary.beans;
 
+import by.bntu.fitr.povt.gapeenko.vlad.web.onlinelibrary.db.Database;
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 
 /**
  *
  * @author Vladislav
  */
 @ManagedBean
-@RequestScoped
-public class Genres {
+@SessionScoped
+public class Genres implements Serializable{
 
-    /**
-     * Creates a new instance of Genres
-     */
+
+    private final ArrayList<Genre> genreList;
+
     public Genres() {
+        this.genreList = new ArrayList<>();
+    }
+
+    private ArrayList<Genre> getGenres() {
+        Statement stmt = null;
+        ResultSet rs = null;
+        Connection conn = null;
+        try {
+            conn = Database.getConnection();
+
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("select * from genre order by name");
+            while (rs.next()) {
+                Genre genre = new Genre();
+                genre.setName(rs.getString("name"));
+                genre.setId(rs.getLong("id"));
+                genreList.add(genre);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Genres.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(Genres.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        return genreList;
+    }
+
+    public ArrayList<Genre> getGenreList() {
+        System.out.println("asdasd");
+        if (!genreList.isEmpty()) {
+            return genreList;
+        } else {
+            return getGenres();
+        }
     }
     
 }
