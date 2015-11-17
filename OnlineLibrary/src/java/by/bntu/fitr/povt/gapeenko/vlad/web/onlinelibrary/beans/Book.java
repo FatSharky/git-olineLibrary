67a8d63f4b96750b3245c7e26cc2f5f67d15e8b8
FatSharky <vladislavbypinsk@mail.ru>
2015-11-17 16:40:45 +0300

    Add field to class book

--- OnlineLibrary/src/java/by/bntu/fitr/povt/gapeenko/vlad/web/onlinelibrary/beans/Book.java
+++ OnlineLibrary/src/java/by/bntu/fitr/povt/gapeenko/vlad/web/onlinelibrary/beans/Book.java
@@ -10,5 +10,16 @@ package by.bntu.fitr.povt.gapeenko.vlad.web.onlinelibrary.beans;
  * @author Vladislav
  */
 public class Book {
+    private long id;
+    private String name;
+    private byte[] content;
+    private int pageCount;
+    private String isbn;
+    private String genre;
+    private String author;
+    private int publishDate;
+    private String publisher;
+    private byte[] image;
+    private String descr;
     
 }
