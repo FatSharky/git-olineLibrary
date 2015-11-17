73480b69770359fea438e6207a7ce2e1750eb1c2
FatSharky <vladislavbypinsk@mail.ru>
2015-11-17 16:41:45 +0300

    Add get and set to fields

--- OnlineLibrary/src/java/by/bntu/fitr/povt/gapeenko/vlad/web/onlinelibrary/beans/Book.java
+++ OnlineLibrary/src/java/by/bntu/fitr/povt/gapeenko/vlad/web/onlinelibrary/beans/Book.java
@@ -21,5 +21,159 @@ public class Book {
     private String publisher;
     private byte[] image;
     private String descr;
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the content
+     */
+    public byte[] getContent() {
+        return content;
+    }
+
+    /**
+     * @param content the content to set
+     */
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+
+    /**
+     * @return the pageCount
+     */
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    /**
+     * @param pageCount the pageCount to set
+     */
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    /**
+     * @return the isbn
+     */
+    public String getIsbn() {
+        return isbn;
+    }
+
+    /**
+     * @param isbn the isbn to set
+     */
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    /**
+     * @return the genre
+     */
+    public String getGenre() {
+        return genre;
+    }
+
+    /**
+     * @param genre the genre to set
+     */
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    /**
+     * @return the author
+     */
+    public String getAuthor() {
+        return author;
+    }
+
+    /**
+     * @param author the author to set
+     */
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    /**
+     * @return the publishDate
+     */
+    public int getPublishDate() {
+        return publishDate;
+    }
+
+    /**
+     * @param publishDate the publishDate to set
+     */
+    public void setPublishDate(int publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    /**
+     * @return the publisher
+     */
+    public String getPublisher() {
+        return publisher;
+    }
+
+    /**
+     * @param publisher the publisher to set
+     */
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    /**
+     * @return the image
+     */
+    public byte[] getImage() {
+        return image;
+    }
+
+    /**
+     * @param image the image to set
+     */
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    /**
+     * @return the descr
+     */
+    public String getDescr() {
+        return descr;
+    }
+
+    /**
+     * @param descr the descr to set
+     */
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
     
 }
