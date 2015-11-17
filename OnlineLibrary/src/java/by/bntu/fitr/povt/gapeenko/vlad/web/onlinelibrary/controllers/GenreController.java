bcf92fe52982269f1e5a0831ae44697f0521be6f
FatSharky <vladislavbypinsk@mail.ru>
2015-11-17 16:49:44 +0300

    Add new controller(GenreController)

---
+++ OnlineLibrary/src/java/by/bntu/fitr/povt/gapeenko/vlad/web/onlinelibrary/controllers/GenreController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package by.bntu.fitr.povt.gapeenko.vlad.web.onlinelibrary.controllers;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author Vladislav
+ */
+@ManagedBean(eager = true)
+@ApplicationScoped
+public class GenreController {
+
+    /**
+     * Creates a new instance of GenreController
+     */
+    public GenreController() {
+    }
+    
+}
