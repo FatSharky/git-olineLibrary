ebdc00a6d515f67be472b0a16710393d21fd697d
FatSharky <vladislavbypinsk@mail.ru>
2015-11-17 17:40:32 +0300

    Select genre from genre by name

--- OnlineLibrary/src/java/by/bntu/fitr/povt/gapeenko/vlad/web/onlinelibrary/controllers/GenreController.java
+++ OnlineLibrary/src/java/by/bntu/fitr/povt/gapeenko/vlad/web/onlinelibrary/controllers/GenreController.java
@@ -5,6 +5,17 @@
  */
 package by.bntu.fitr.povt.gapeenko.vlad.web.onlinelibrary.controllers;
 
+import by.bntu.fitr.povt.gapeenko.vlad.web.onlinelibrary.beans.Genre;
+import by.bntu.fitr.povt.gapeenko.vlad.web.onlinelibrary.beans.Genres;
+import by.bntu.fitr.povt.gapeenko.vlad.web.onlinelibrary.db.Database;
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 
@@ -14,12 +25,61 @@ import javax.faces.bean.ManagedBean;
  */
 @ManagedBean(eager = true)
 @ApplicationScoped
-public class GenreController {
+public class GenreController implements Serializable {
+
+    private ArrayList<Genre> genreList;
 
     /**
      * Creates a new instance of GenreController
      */
     public GenreController() {
+        fillGenresAll();
     }
-    
+
+    private void fillGenresAll() {
+        Statement stmt = null;
+        ResultSet rs = null;
+        Connection conn = null;
+
+        genreList = new ArrayList<Genre>();
+        try {
+            conn = Database.getConnection();
+
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("select * from genre by name");
+            while (rs.next()) {
+                Genre genre = new Genre();
+                genre.setName(rs.getString("name"));
+                genre.setId(rs.getLong("id"));
+                getGenreList().add(genre);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(GenreController.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(Genres.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+
+    }
+
+    /**
+     * @return the genreList
+     */
+    public ArrayList<Genre> getGenreList() {
+        return genreList;
+    }
+
 }
