97f85571bf853e724c20706a57347fd4be1af08f
FatSharky <vladislavbypinsk@mail.ru>
2015-12-12 14:37:18 +0300

    add function showProgress

--- OnlineLibrary/web/JS/library.js
+++ OnlineLibrary/web/JS/library.js
@@ -4,4 +4,22 @@
  * and open the template in the editor.
  */
 
+function checkValue (form, message){
+    var userIput = form[form.id + ":username"];
+    
+    if (userInput.vlaue === ''){
+        alert(message);
+        usuerIput.focus();
+        return false;
+    }
+    return true;
+}
 
+function showProgess(data){
+    if(data.status === "begin"){
+        document.getElemetById('loading_wrapper').style.display="block";   
+    }
+    else if (data.status==="success"){
+        document.getElemetById('loading_wrapper').style.display="nine";
+    }
+}
